a3829b3626bda01b487cf6b5733bf59c093dd3a8
abhishek <abhishek.khaitan@waiward.com>
2015-06-23 11:10:05 -0600

    Committing missing files

---
+++ HseqCentralApp/Models/Par.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HseqCentralApp.Models
+{
+
+    //Preventive Action record
+    public class Par : HseqRecord
+    {
+    
+        public Par() { }
+
+        public Par(HseqRecord hseqRecord) : base(hseqRecord) { }
+
+         public String status { get; set; }
+
+    }
+}
---
+++ HseqCentralApp/Models/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HseqCentralApp.Models
+{
+    public class Test : HseqRecord
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public HseqRecord linkRecord { get; set; }
+
+    }
+}
