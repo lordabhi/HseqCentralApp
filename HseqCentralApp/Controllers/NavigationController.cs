cca6413f281f451217f7b69ed1effbe3443ef80d
abhishek <abhishek.khaitan@waiward.com>
2015-08-06 16:30:49 -0600

    Added Approval Edit Form

--- HseqCentralApp/Controllers/NavigationController.cs
+++ HseqCentralApp/Controllers/NavigationController.cs
@@ -244,6 +244,50 @@ namespace HseqCentralApp.Controllers
             return PartialView("_MainContentCallbackPanel");
         }
 
+        
+        [HttpPost, ValidateInput(false)]
+        public ActionResult ApprovalGridViewUpdate(HseqApprovalRequest item)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (item != null)
+                    {
+                        db.Entry(item).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    // Retrieve the error messages as a list of strings.
+                    var errorMessages = ex.EntityValidationErrors
+                            .SelectMany(x => x.ValidationErrors)
+                            .Select(x => x.ErrorMessage);
+
+                    // Join the list to a single string.
+                    var fullErrorMessage = string.Join("; ", errorMessages);
+
+                    // Combine the original exception message with the new one.
+                    var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
+
+                    // Throw a new DbEntityValidationException with the improved exception message.
+                    throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            else
+            {
+                var errors = ModelState.Values.SelectMany(v => v.Errors);
+                ViewData["EditError"] = "Please, correct all errors.";
+                return PartialView("_ApprovalEditView", item);
+            }
+            return PartialView("_MainContentCallbackPanel");
+        }
+
 
         [HttpPost, ValidateInput(false)]
        // [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
